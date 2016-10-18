fab12034f7748536f1e449b98b8bd0d4c11039af
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2016-10-18 16:00:08 +0200

    Added properties for Place

--- Demo UI/Place.swift
+++ Demo UI/Place.swift
@@ -6,6 +6,18 @@
 //  Copyright © 2016 Tec-Tec. All rights reserved.
 //
 
+import Foundation
+
 struct Place {
-    
+
+    let name: String
+    let adress: String
+    let phoneNumber: String?
+    let websiteURL: URL?
+    let wikipediaURL: URL?
+    let note: Float
+    let numberOfReviews: Int
+
+    let latitude: Double
+    let longitude: Double
 }

79cfdc551483c92404b462f690719ea6539552ad
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2016-10-18 12:09:09 +0200

    Initial Commit

---
+++ Demo UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo UI
+//
+//  Created by Ludovic Ollagnier on 18/10/2016.
+//  Copyright © 2016 Tec-Tec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

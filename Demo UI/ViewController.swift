d802e8b57cb751de1859984a70b82148ac82056f
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2016-10-18 17:47:27 +0200

    Added a way to get a place from the form, with integrity checks

--- Demo UI/ViewController.swift
+++ Demo UI/ViewController.swift
@@ -25,6 +25,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var reviewSlider: UISlider!
     @IBOutlet var coordinateTextFields: [UITextField]!
 
+    private var placeFromForm: Place? {
+        guard let name = nameTextField.text else { return nil }
+        guard name.characters.count > 2 else { return nil }
+        guard let address = addressTextField.text else { return nil}
+        guard address.characters.count > 2 else { return nil }
+        guard let lat = latitudeTextField.text, let latDouble = Double(lat) else { return nil}
+        guard let long = longitudeTextField.text, let longDouble = Double(long) else { return nil}
+        guard let websiteUrlString = websiteTextField.text else { return nil }
+        guard let wikipediaUrlString = wikiLinkTextField.text else { return nil }
+
+        let place = Place(name: name, address: address, phoneNumber: phoneTextField.text, websiteURL: URL(string: websiteUrlString), wikipediaURL: URL(string: wikipediaUrlString), note: reviewSlider.value, numberOfReviews: 1, latitude: latDouble, longitude: longDouble)
+        return place
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -34,14 +48,14 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
     @IBAction func savePlace(_ sender: AnyObject) {
     }
 
     @IBAction func cancel(_ sender: AnyObject) {
     }
-    
-    @IBAction func autoCoordinatesSwitchValueChanged(_ sender: UISwitch) {
 
+    @IBAction func autoCoordinatesSwitchValueChanged(_ sender: UISwitch) {
         for tf in coordinateTextFields {
             tf.isEnabled = !sender.isOn
         }

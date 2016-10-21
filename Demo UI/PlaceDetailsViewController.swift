bb79f5899abcb514e1a22715749f84579f2bb63e
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2016-10-21 09:43:37 +0200

    Added a subclass for the details

---
+++ Demo UI/PlaceDetailsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PlaceDetailsViewController.swift
+//  Demo UI
+//
+//  Created by Ludovic Ollagnier on 21/10/2016.
+//  Copyright © 2016 Tec-Tec. All rights reserved.
+//
+
+import UIKit
+
+class PlaceDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
